d36855c5103e1c5dc9393ae32b4a8cec24e5baa9
robsonapalmeida <robsonsgbl@gmail.com>
2019-06-24 22:45:24 -0300

    finally, the lin_to for SERVICE in the middle of the page has worked

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
    def index
    end
+
+   def new
+     @teste = 'Passou no controller corretamente'   
+   end
    
    def calendar
      @Calendar = 'Teste call calendar'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'spa'  => 'welcome#index' 
+  root 'welcome#index'
+  #get 'spa'  => 'welcome#index' 
   get 'calendar' => 'welcome#calendar'    
     
  end
