bd0b08f0699502508d245d395643f1a38f96d360
robsonapalmeida <robsonsgbl@gmail.com>
2019-06-26 19:28:04 -0300

    Eliminate changes made for wrong use of bootstrapdatepicker

--- app/helpers/user_x_services_helper.rb
+++ app/helpers/user_x_services_helper.rb
@@ -1,2 +1,8 @@
 module UserXServicesHelper
+    def datepicker_input form, field
+        content_tag :td, :data => {:provide => 'datepicker', 'date-format' => 'yyyy-mm-dd', 'date-autoclose' => 'true'} do
+          form.text_field field, class: 'form-control', placeholder: 'YYYY-MM-DD'
+        end
+      end
+
 end
