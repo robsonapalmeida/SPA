5a2016d0d96b385ecece4f48f34e1ebe51e52506
Robson <robsonsgbl@gmail.com>
2019-09-10 08:42:11 -0300

    fix assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,10 +8,9 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Precompile additional assets.
-application.js, application.css, and all non-JS/CSS in the app/assets
-folder are already added.
-
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
 Rails.application.config.assets.precompile += %w( estilo.css
                                                   mesoterapia.css
                                                   microdermoabrasion.css
@@ -23,7 +22,7 @@ Rails.application.config.assets.precompile += %w( estilo.css
 
  
 
-Rails.application.config.assets.precompile += %w( cable.js 
+Rails.application.config.assets.precompile += %w( cable.js
                                                   application.js
 
                                                   )
